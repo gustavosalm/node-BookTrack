56d813a27f0f326a2b1c2cdd9a2649d460ab6f1a
gustavosalm <gustavosalmeida9821@gmail.com>
2025-05-11 16:36:00 -0300

    feat: users route (get, post)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../config/db');
+
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.all('SELECT id, nome, email FROM usuarios', [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(`fetch users error: ${err.message}`);
+    }
+    res.json(rows);
+  });
+});
+
+router.post('/', async (req, res) => {
+  const { nome, email, senha } = req.body;
+  const passwordHash = await bcrypt.hash(senha, 10);
+
+  db.run(`INSERT INTO usuarios (nome, email, senha)
+          VALUES (?, ?, ?)`,
+      [nome, email, passwordHash],
+      function(err) {
+          if (err) {
+              return res.status(400).json({ error: err.message });
+          }
+          res.status(201).json({ sucess: true });
+      });
 });
 
 module.exports = router;
