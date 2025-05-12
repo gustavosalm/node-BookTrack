cfbb8a53aa8d8055bf674c95efd31f8bd55ada00
gustavosalm <gustavosalmeida9821@gmail.com>
2025-05-12 16:30:25 -0300

    feat: added user login route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../config/db');
+const jwt = require('jsonwebtoken');
+
+// Lembrar de por em um .env
+const SECRET = "chave";
 
 var router = express.Router();
 
@@ -13,9 +17,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
   const { nome, email, senha } = req.body;
-  const passwordHash = await bcrypt.hash(senha, 10);
+  const passwordHash = await bcrypt.hash(senha, 12);
+
+  if (!nome || nome.length < 3 || !email || !senha) {
+    return res.status(400).json({ error: "Dados inválidos." });
+  }
 
   db.run(`INSERT INTO usuarios (nome, email, senha)
           VALUES (?, ?, ?)`,
@@ -24,9 +32,33 @@ router.post('/', async (req, res) => {
           if (err) {
               return res.status(400).json({ error: err.message });
           }
-          res.status(201).json({ sucess: true });
+          res.status(201).json({ sucess: true, id: this.lastID });
       });
 });
+
+router.post('/login', async (req, res) => {
+  const { email, senha } = req.body;
+
+  db.get(
+    `SELECT * FROM usuarios WHERE email = ?`,
+    [email],
+    async function(err, user) {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      if (!user){
+        return res.status(400).json({ error: "Email ou senha inválidos." });
+      }
+
+      const valid = await bcrypt.compare(senha, user.senha);
+      if (!valid)
+        return res.status(400).json({ error: 'E-mail ou senha inválidos' });
+      
+      const token = jwt.sign({ id: user.id, name: user.nome, email: user.email }, SECRET, { expiresIn: '1d' });
+      res.status(200).json({ success: true, token });
+    }
+  )
+})
 
 router.delete('/', async (req, res) => {
   const { usuario_id } = req.body;
