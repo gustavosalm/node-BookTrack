98392b887c2013e631dac84038496e04cba1590e
gustavosalm <gustavosalmeida9821@gmail.com>
2025-05-12 14:37:14 -0300

    feat: edit book grade

--- routes/books.js
+++ routes/books.js
@@ -48,21 +48,47 @@ router.put('/status', async (req, res) => {
             res.status(200).json({ sucess: true });
         }
     )
-})
+});
 
 router.delete('/', async (req, res) => {
     const { livro_id } = req.body;
 
     db.run(
-    `DELETE FROM livros WHERE id=?`,
-    [livro_id],
-    function(err) {
-      if (err) {
-        return res.status(400).json({error: err.message });
-      }
-      res.status(204).json({ success: true });
-    }
-  );
-})
+        `DELETE FROM livros WHERE id=?`,
+        [livro_id],
+        function(err) {
+            if (err) {
+                return res.status(400).json({error: err.message });
+            }
+            res.status(204).json({ success: true });
+        }
+    );
+});
+
+router.put('/grade', async (req, res) => {
+    const { livro_id, nota } = req.body;
+
+    db.get(
+        `SELECT status FROM livros WHERE id = ?`,
+        [livro_id], 
+        (err, row) => {
+            if (err)
+                return res.status(400).json({ error: "Erro ao buscar livro." });
+
+            if (row.status !== "Lido")
+                return res.status(400).json({ error: "Só é possível avaliar livros com status 'Lido'." });
+
+            db.run(
+                `UPDATE livros SET avaliacao = ? WHERE id = ?`,
+                [nota, livro_id],
+                function (err) {
+                    if (err)
+                        return res.status(500).json({ error: "Erro ao atualizar a avaliação." });
+                    res.json({ message: "Avaliação atualizada com sucesso." });
+                }
+            );
+        }
+    );
+});
 
 module.exports = router;
